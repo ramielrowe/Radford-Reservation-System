24e4c3aa09f012a3a30463a2bf2f49d00d2c404d
Andrew Melton <ramielrowe@gmail.com>
2011-11-01 11:22:18 -0400

    Moving the office hours calendar to a separate file.

--- src/home.php
+++ src/home.php
@@ -86,34 +86,12 @@ while($row = mysql_fetch_assoc($resresult)){
 	
 }
 
+require 'hourscalendar.php';
+
 $homepage = "
 	<center>
 	$messages
-		<h3><br />Office Hours</h3>
-		<table class=\"pickuptimes\">
-			<tr>
-				<td class=\"header\">Monday</td>
-				<td class=\"header\">Tuesday</td>
-				<td class=\"header\">Wednesday</td>
-				<td class=\"header\">Thursday</td>
-				<td class=\"header\">Friday</td>
-			</tr>
-			<tr>
-				<td class=\"centeredcellbold\">3pm-5:30pm</td>
-				<td class=\"centeredcellbold\">10am-12pm</td>
-				<td class=\"centeredcellbold\">1pm-5:30pm</td>
-				<td class=\"centeredcellbold\">8:30am-10am</td>
-				<td class=\"centeredcellbold\">3pm-5pm</td>
-			</tr>
-			<tr>
-				<td class=\"centeredcellbold\">-</td>
-				<td class=\"centeredcellbold\">3:30pm-5:30pm</td>
-				<td class=\"centeredcellbold\">-</td>
-				<td class=\"centeredcellbold\">3:30pm-5:30pm</td>
-				<td class=\"centeredcellbold\">-</td>
-			</tr>
-
-		</table>
+		$hourscalendar
 		
 		<h3>Your Equipment</h3>
 		
--- src/login.php
+++ src/login.php
@@ -27,6 +27,8 @@ else if($loginoption == RES_ERROR_LOGIN_USER_PASS){
 
 $url = (!empty($_SERVER['HTTPS'])) ? "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] : "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 
+require 'hourscalendar.php';
+
 $loginpage = $loginpage . "
 	
 		<center><h3>Welcome!</h3>
@@ -52,38 +54,7 @@ $loginpage = $loginpage . "
 			</table>
 		
 		</form>
-		<center><h3>Pickup/Dropoff Times</h3></center>
-		<table class=\"pickuptimes\">
-			<tr>
-				<td class=\"header\">Monday</td>
-				<td class=\"header\">Tuesday</td>
-				<td class=\"header\">Wednesday</td>
-				<td class=\"header\">Thursday</td>
-				<td class=\"header\">Friday</td>
-			</tr>
-			<tr>
-				<td class=\"centeredcellbold\">10am-12pm</td>
-				<td class=\"centeredcellbold\">10am-12pm</td>
-				<td class=\"centeredcellbold\">9am-12pm</td>
-				<td class=\"centeredcellbold\">11am-12pm</td>
-				<td class=\"centeredcellbold\">10am-12pm</td>
-			</tr>
-			<tr>
-				<td class=\"centeredcellbold\">-</td>
-				<td class=\"centeredcellbold\">1pm-2pm</td>
-				<td class=\"centeredcellbold\">2pm-4pm</td>
-				<td class=\"centeredcellbold\">1pm-2pm</td>
-				<td class=\"centeredcellbold\">-</td>
-			</tr>
-			<tr>
-				<td class=\"centeredcellbold\">-</td>
-				<td class=\"centeredcellbold\">3pm-4pm</td>
-				<td class=\"centeredcellbold\">-</td>
-				<td class=\"centeredcellbold\">3pm-4pm</td>
-				<td class=\"centeredcellbold\">-</td>
-			</tr>
-
-		</table>
+		$hourscalendar;
 	
 	</center>
 
